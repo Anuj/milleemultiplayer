3f2355820312847cc713ec634c69d3e37f4b873a
preddy87 <preddy87@184bbdbc-f548-11dd-ada4-5153b8187bf2>
2009-02-16 11:55:27 +0000

    Update: Network.java wasn't added in previous commit.
    Added networking to the code (moved all networking elements to Network class).  For now, it assumes only 2 players are playing.    Right now, each player initializes his own player's location and sends it to other phone.  Server initializes the tokens' locations and sends to clients.  Each players actions are appropriately sent to other device - a small lag, but otherwise ok.
    
    Note: there is a lot of extra commented out code and messy code at this point.  But, I wanted to commit so I wouldn't lose this code.  I'll be fixing this messy code in the next revision as well as generalizing it to allow for n players.

---
+++ IMApplication/trunk/src/Network.java
@@ -0,0 +1,122 @@
+import java.util.Vector;
+
+import javax.microedition.io.StreamConnection;
+
+
+public class Network implements Runnable {
+	
+	ClientServer clientServer = null;
+	private boolean m_bRunThread = true;
+	private boolean isServer;
+	private int numClients;
+	Vector sendBuffer, receiverBuffer;
+	SenderThread sendThread = null;
+	ReceiverThread recvThread = null;
+	StreamConnection[] streamConns;
+	
+	public Object connected = new Object();
+	
+	public Network () {
+		
+	}
+	
+	public void initializeNetwork(boolean isServer, int numClients) {
+		this.isServer = isServer;
+		this.numClients = numClients;
+		System.out.println("initialized network class");
+		sendBuffer = new Vector();
+		receiverBuffer = new Vector();
+
+    	clientServer = new ClientServer(isServer, numClients);
+	}
+	
+	public void run() {
+        //while (m_bRunThread) {
+            try {
+            	System.out.println("inside Network.  trying to connect client");
+            	// Just by creating a new Application object, you run the application
+            	// either as a client or server depending on the m_bIsServer variable.
+                /*if (app == null) {
+                	app = new IMApplication(m_bIsServer, 1);
+                	//exitMIDlet();
+                }*/
+            	
+            	if (isServer) {
+        			//this.numClients = numClients;
+        			clientServer.InitServer(numClients);
+        		} else {
+        			System.out.println("about to initClient()");
+        			clientServer.InitClient();
+        			try {
+        				// need to manually wait for client to be connected to server
+        				// because client doesn't block until it's connected.
+        				synchronized(clientServer.connected) {
+        					clientServer.connected.wait();
+        				}
+        			} catch (Exception e) {
+        				e.printStackTrace();
+        			}
+        		}
+            	
+            	
+        		if (isServer) {
+        			streamConns = clientServer.getStreamConnections();
+        			sendThread = new SenderThread(streamConns);
+        			recvThread = new ReceiverThread(streamConns[0], sendThread, isServer);
+        			recvThread.start();
+        	    	sendThread.start();
+
+        		} else {
+        			streamConns = clientServer.getStreamConnections();
+        			sendThread = new SenderThread(streamConns);
+        			recvThread = new ReceiverThread(streamConns[0], sendThread, isServer);
+        			recvThread.start();
+        	    	sendThread.start();
+        		}
+                
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+ 
+        // }
+	}
+	
+	public void send(String msg) {
+		String finalMsg = msg.concat("\0");
+		sendThread.sendMsg(finalMsg, new Integer(-1));
+	}
+	
+	public String receive() {
+		String msg = null;
+		
+		while (msg == null) {
+			if ((msg = clientServer.receiveMessage(recvThread)) != null) {
+				System.out.println("msg received: " + msg);
+				msg = msg.substring(0, msg.length()-1);
+			}
+		}
+		
+		return msg;
+	}
+	
+	public void sendReceive() {
+		/*System.out.println("connected!");
+    	//connected.notifyAll();
+    	
+    	
+		if (isServer) {
+			sendThread.sendMsg("u", new Integer(-1));
+			sendThread.sendMsg("d", new Integer(-1));
+			sendThread.sendMsg("r", new Integer(-1));
+			sendThread.sendMsg("l", new Integer(-1));
+		} else {
+			while (!exitFlag) {
+				if ((msg = clientServer.receiveMessage(recvThread)) != null) {
+					System.out.println("msg received: " + msg);
+				}
+				
+				//sendThread.sendMsg("sending: blah blah", new Integer(-1));
+			}
+		}*/
+	}
+}
