acc3474a421deb87b334a370e84c9d212a3f3f29
simtanx <simtanx@184bbdbc-f548-11dd-ada4-5153b8187bf2>
2009-03-04 10:16:28 +0000

    Tagging a cooperative version.

---
+++ IMApplication/tags/cooperative_1.0.3/src/millee/game/mechanics/GameCell.java
@@ -0,0 +1,57 @@
+package millee.game.mechanics;
+
+import java.util.Vector;
+
+public class GameCell {
+	
+	private Vector _occupants = new Vector();
+	private Goodie _item = null;
+
+	public GameCell() {
+		/*
+		if (players == null) {
+			_occupants = new Vector();
+		}
+		else {
+			_occupants = new Vector(players.length);
+			for (int i = 0; i < players.length; i++) {
+				_occupants.addElement(players[i]);
+			}
+		}
+		
+		
+		_item = g;
+		*/
+	}
+	
+	public void addPlayer(Player p) {
+		_occupants.addElement(p);
+	}
+	
+	public void removePlayer(Player p) {
+		_occupants.removeElement(p);
+	}
+	
+	public boolean hasPlayer() {
+		return !(_occupants.size() == 0);
+	}
+	
+	
+	public boolean hasGoodie() {
+		return (_item != null);
+	}
+	
+	public void setGoodie(Goodie g) {
+		_item = g;
+	}
+	
+	public void unsetGoodie() {
+		_item.sprite.setVisible(false);
+		_item = null;
+	}
+	
+	public Goodie getGoodie() {
+		return _item;
+	}
+
+}
