a9c9672d888997cbf882bc5f2e87c09fba354883
simtanx <simtanx@184bbdbc-f548-11dd-ada4-5153b8187bf2>
2009-03-07 23:58:46 +0000

    Setting up a branch to try microlog

---
+++ IMApplication/branches/microlog_trial/src/millee/game/LevelStartPage.java
@@ -0,0 +1,48 @@
+package millee.game;
+
+
+import javax.microedition.lcdui.StringItem;
+
+import millee.game.initialize.Screen;
+import millee.network.Network;
+
+
+public class LevelStartPage extends Screen {
+	
+	Network network;
+	int characterChoice;
+	private ApplicationMain _app;
+
+	public LevelStartPage(String title, Network network, int characterChoice, boolean isServer, String myName, String myImagePath, ApplicationMain app) {
+		super(title);
+		// TODO Auto-generated constructor stub
+		
+		StringItem str = new StringItem("Colour Colour", "Level 1: Colours");
+		StringItem str2 = new StringItem(null, "Ready! Set! Go!");
+		
+		this.characterChoice = characterChoice;
+		this.network = network;
+		//network.sendReceive();
+		
+		this._app = app;
+		
+		this.append(str);
+		this.append(str2);
+		this.addCommand(exitCommand);
+		
+		if (isServer) {
+			this.append("Press START to begin the next level!");
+			this.addCommand(startCommand);
+			System.out.println("after adding command");
+		} else {
+			//this.addCommand(startCommand);
+			this.append("Waiting for server to start the game...");
+			
+			//sendPlayerInfo(myName, myImagePath);
+		}
+	}
+	
+	public void addMessage(String msg) {
+		this.append(msg);
+	}
+}
