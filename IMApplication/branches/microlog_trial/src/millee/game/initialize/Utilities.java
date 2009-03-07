6985cee20c73e3cee059747587d022bb84c9f3d1
simtanx <simtanx@184bbdbc-f548-11dd-ada4-5153b8187bf2>
2009-03-07 23:58:46 +0000

    Setting up a branch to try microlog

---
+++ IMApplication/branches/microlog_trial/src/millee/game/initialize/Utilities.java
@@ -0,0 +1,151 @@
+package millee.game.initialize;
+
+import java.io.IOException;
+import java.util.Vector;
+
+import javax.microedition.lcdui.Graphics;
+import javax.microedition.lcdui.Image;
+
+public class Utilities {
+	
+	public static final String DEFAULT_IMAGE = "/dancer_0.png";
+
+	public static Image createImage(String path) {
+		try {
+			return Image.createImage(path);
+		}
+        catch (IOException e) {
+        	System.err.println("IOException: " + e);
+        	try {
+				return Image.createImage(DEFAULT_IMAGE);
+			} catch (IOException e1) {
+				e1.printStackTrace();
+				return null;
+			}
+        }
+	}
+	
+	/**
+	 * A class for splitting strings in J2ME applications as can be done by 
+	 * using java.lang.String#split() in J2SE.
+	 *
+	 * @author Lasse Koskela
+	 * A method for splitting a string in J2ME.
+	 *
+	 * @param splitStr The string to split.
+	 * @param delimiter The characters to use as delimiters.
+	 * @param limit The result threshold as documented in 
+	 *              java.lang.String#split(String, int).
+	 * @return An array of strings.
+	 */
+	public static String[] split(String splitStr, String delimiter, int limit) {
+	        // some input validation / short-circuiting
+	        if (delimiter == null || delimiter.length() == 0)
+	        {
+	            return new String[] { splitStr };
+	        }
+	        else if (splitStr == null)
+	        {
+	            return new String[0];
+	        }
+	        
+	        // enabling switches based on the 'limit' parameter
+	        boolean arrayCanHaveAnyLength = false;
+	        int maximumSplits = Integer.MAX_VALUE;
+	        boolean dropTailingDelimiters = true;
+	        if (limit < 0)
+	        {
+	            arrayCanHaveAnyLength = true;
+	            maximumSplits = Integer.MAX_VALUE;
+	            dropTailingDelimiters = false;
+	        }
+	        else if (limit > 0)
+	        {
+	            arrayCanHaveAnyLength = false;
+	            maximumSplits = limit - 1;
+	            dropTailingDelimiters = false;
+	        }
+	        
+	        StringBuffer token = new StringBuffer();
+	        Vector tokens = new Vector();
+	        char[] chars = splitStr.toCharArray();
+	        boolean lastWasDelimiter = false;
+	        int splitCounter = 0;
+	        for (int i = 0; i < chars.length; i++)
+	        {
+	            // check for a delimiter
+	            if (i + delimiter.length() <= chars.length && splitCounter < maximumSplits)
+	            {
+	                String candidate = new java.lang.String(chars, i, delimiter.length());
+	                if (candidate.equals(delimiter))
+	                {
+	                    tokens.addElement(token.toString());
+	                    token.setLength(0);
+	                    
+	                    lastWasDelimiter = true;
+	                    splitCounter++;
+	                    i = i + delimiter.length() - 1;
+	                    
+	                    continue; // continue the for-loop
+	                }
+	            }
+	            
+	            // this character does not start a delimiter -> append to the token
+	            token.append(chars[i]);
+	            lastWasDelimiter = false;
+	        }
+	        // don't forget the "tail"...
+	        if (token.length() > 0 || (lastWasDelimiter && !dropTailingDelimiters))
+	        {
+	            tokens.addElement(token.toString());
+	        }
+	        // convert the vector into an array
+	        String[] splitArray = new String[tokens.size()];
+	        for (int i = 0; i < splitArray.length; i++)
+	        {
+	            splitArray[i] = (String) tokens.elementAt(i);
+	        }
+	        return splitArray;
+	    }
+	    
+	    /**
+	     * This method resizes an image by resampling its pixels
+	     * @param src The image to be resized
+	     * @param screenWidth The width to resize to fit
+	     * @param screenHeight The height to resize to fit
+	     * @return The resized image
+	     */
+
+	     public static Image resizeImage(Image src, int screenWidth, int screenHeight) {
+	         int srcWidth = src.getWidth();
+	         int srcHeight = src.getHeight();
+	         Image tmp = Image.createImage(screenWidth, srcHeight);
+	         Graphics g = tmp.getGraphics();
+	         int ratio = (srcWidth << 16) / screenWidth;
+	         int pos = ratio/2;
+
+	         //Horizontal Resize        
+
+	         for (int x = 0; x < screenWidth; x++) {
+	             g.setClip(x, 0, 1, srcHeight);
+	             g.drawImage(src, x - (pos >> 16), 0, Graphics.LEFT | Graphics.TOP);
+	             pos += ratio;
+	         }
+
+	         Image resizedImage = Image.createImage(screenWidth, screenHeight);
+	         g = resizedImage.getGraphics();
+	         ratio = (srcHeight << 16) / screenHeight;
+	         pos = ratio/2;        
+
+	         //Vertical resize
+
+	         for (int y = 0; y < screenHeight; y++) {
+	             g.setClip(0, y, screenWidth, 1);
+	             g.drawImage(tmp, 0, y - (pos >> 16), Graphics.LEFT | Graphics.TOP);
+	             pos += ratio;
+	         }
+	         return resizedImage;
+
+	     }
+
+}
---
+++ IMApplication/branches/microlog_trial/src/millee/game/mechanics/Player.java
@@ -0,0 +1,235 @@
+package millee.game.mechanics;
+import java.util.Hashtable;
+import java.util.Stack;
+
+import javax.microedition.lcdui.Image;
+import javax.microedition.lcdui.game.Sprite;
+
+import millee.game.initialize.Utilities;
+
+
+public class Player {
+	private String _name;
+	
+	// Location in cell coordinates
+	protected int x;
+	protected int y;
+	
+	// Sprite must have its own coordinates
+	protected Sprite sprite;
+	
+	// Assigned color to collect -- TODO: Are these related?
+	private int _assignedColor;
+	private int _id;
+	
+	// Player keeps track of his own score & goodies collected
+	private int _score = 0;
+	private GoodieStack _goodies;
+	
+	// Player sprite color variations
+	public static final int BLACK = 1;
+	public static final int RED = 2;
+	public static final int GREEN = 3;
+	public static final int BLUE = 4;
+	
+	private static Hashtable avatarUsageCounts = new Hashtable();
+	
+	/** virtualID is determined by the server, depending on what order the client
+	 * sends their initial message.  physicalID is the order that the clients join. 
+	 * @param name
+	 * @param avatar
+	 * @param virtualID
+	 */
+	public Player(String name, String imgPath, int virtualID, boolean isLocal) { //, int physicalID) {
+		
+		this._name = name;
+		this._id = virtualID;
+		
+		// The color that this character must collect is its ID+1
+		this._assignedColor = virtualID+1;
+		_goodies = new GoodieStack(_assignedColor);
+		
+		Image avatar = null;
+		
+		if (isLocal) {
+			avatar = Utilities.createImage(imgPath);
+		}
+		else {
+			// Ignore the chosen avatar...
+			avatar = Utilities.createImage(Utilities.DEFAULT_IMAGE);
+		}
+		
+		// Keep track of avatar usage counts
+		int nUsage = 1; // Default
+		if (avatarUsageCounts.containsKey(imgPath)) {
+			// Get and increment value
+			nUsage = ((Integer) avatarUsageCounts.get(imgPath)).intValue();
+			nUsage++;
+		}
+		avatarUsageCounts.put(imgPath, new Integer(nUsage));
+
+		// Alter color if necessary
+		//if (nUsage > 1) {
+		//	sprite = new Sprite(applyVariation(avatar,nUsage));
+		//}
+		//else {
+			sprite = new Sprite(avatar);
+		//}
+	}
+	
+	private Image applyVariation(Image img, int var) {
+		// Pull out ARGB data
+		int[] rgbData = new int[img.getWidth()*img.getHeight()];
+		img.getRGB(rgbData, 0, img.getWidth(), 0, 0, img.getWidth(), img.getHeight());
+		
+		// Affect the ARGB data according to the variation type
+		switch (var) {
+		
+		// TODO: Verify color change works; otherwise, just use preset images
+		
+		case BLUE:
+			for (int i = 0; i < rgbData.length; i++) {
+				//rgbData[i] = (30 + (rgbData[i] & 0xFF)) | (rgbData[i] & 0xFFFFFF00);
+				rgbData[i] = rgbData[i] & 0xFF0000FF;
+			}
+			break;
+			
+		case GREEN:
+			for (int i = 0; i < rgbData.length; i++) {
+				//rgbData[i] = (30 + (rgbData[i] & 0xFF)) | (rgbData[i] & 0xFFFFFF00);
+				rgbData[i] = rgbData[i] & 0xFF00FF00;
+			}
+			break;
+			
+		case RED:
+			for (int i = 0; i < rgbData.length; i++) {
+				//rgbData[i] = (30 + (rgbData[i] & 0xFF)) | (rgbData[i] & 0xFFFFFF00);
+				rgbData[i] = rgbData[i] & 0xFFFF0000;
+			}
+			break;
+		}
+		
+        /** Alternative: Replace only a particular color in the original avatar (like costume changing)
+        for (int j = 0; j < maskedOldColor.length; j++) {
+            if (currentMaskedPixel == maskedOldColor[j]) {
+                raw[i] = (currentPixel & alphaOnlyBitmask) | maskedNewColor[j];
+                break;
+            }
+        }
+		**/
+		
+		return Image.createRGBImage(rgbData, img.getWidth(), img.getHeight(), true);
+	}
+
+	public int getID() {
+		return _id;
+	}
+	
+	/** Avoid using this
+	public void setPosition(int inX, int inY) {
+		this.x = inX;
+		this.y = inY;
+	}
+	*/
+	
+//	public void interpretMove() {
+//		if (!finishedRound) {
+//			char command = getCommand();
+//			
+//			switch (command) {
+//				case 'U':
+//					y--;
+//					sprite.move(0, -15);
+//					break;
+//				case 'D':
+//					y++;
+//					sprite.move(0, 15);
+//					break;
+//				case 'R':
+//					x++;
+//					sprite.move(15, 0);
+//					break;
+//				case 'L':
+//					x--;
+//					sprite.move(-15, 0);
+//					break;
+//			}
+//
+//			/*
+//			if (command == 'U')
+//				sprite.setPosition(x, y-1);
+//			else if (command == 'D')
+//				sprite.setPosition(x, y+1);
+//			else if (command == 'R')
+//				sprite.setPosition(x+1, y);
+//			else if (command == 'L')
+//				sprite.setPosition(x-1, y);
+//				
+//				*/
+//		}
+//	}
+//	
+//	private char getCommand() {
+//		int index = random.nextInt(4);
+//		return _possibleCommands[index];
+//	}
+	
+	public Sprite getSprite() {
+		return sprite;
+	}
+	
+	// Get and set this player's assigned color
+	public void setColor(int color) {
+		_assignedColor = color;
+	}
+	
+	public int assignedColor() {
+		return _assignedColor;
+	}
+	
+	// Update the player's score in game-like ways
+	private void incrementScore() {
+		_score += 10;
+	}
+	
+	private void decrementScore() {
+		_score -= 10;
+	}
+	
+	public int getScore() {
+		return _score;
+	}
+	
+	public String getName() {
+		return _name;
+	}
+	
+	public void collect(Goodie g) {
+		_goodies.push(g);
+		if (g.getType() == this._assignedColor) { this.incrementScore(); }
+		else { this.decrementScore(); }
+	}
+	
+	public Goodie dropGoodie() {
+		if (_goodies.isEmpty()) { return null; }
+		return (Goodie) _goodies.pop();
+	}
+	
+	public GoodieStack getGoodieStack() {
+		return _goodies;
+	}
+	
+	public void flushGoodieStack() {
+		_goodies = new GoodieStack(_assignedColor);
+	}
+	
+	public boolean hasCorrectGoodies() {
+		Goodie g = null;
+		for (int i = 0; i < _goodies.size(); i++) {
+			g = (Goodie) _goodies.elementAt(i);
+			if (g.getType() != this._assignedColor) { return false; }
+		}
+		
+		return true;
+	}
+}
